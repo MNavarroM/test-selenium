0d99467d92c606372f716a9e0f92d4f65c68b7d8
Mario Navarro <navarromario1@gmail.com>
2023-09-24 18:04:31 +0200

    primer commit

---
+++ src/main/java/selenium/TestSelenium.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class TestSelenium {
+
+	public TestSelenium() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
